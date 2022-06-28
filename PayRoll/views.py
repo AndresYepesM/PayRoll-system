6abf953b6175a8e89a0d875fed22662854abf567
AndresYepesM <aymoreno567@gmail.com>
2022-06-28 11:09:16 -0400

    Templates, login form, home template

---
+++ PayRoll/views.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages, auth
+from datetime import date
+from random import *
+
+
+def home(request):
+    return render(request, 'mains/home.html')
